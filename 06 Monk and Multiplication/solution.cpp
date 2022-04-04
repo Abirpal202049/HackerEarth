52cbd77749fe019ce3ec31a12e9d62037a0be828
Abir Pal <abirpal282002@gmail.com>
2022-04-04 21:39:20 +0530

    Monk and Multiplication

---
+++ 06 Monk and Multiplication/solution.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    int defaultValue = -1;
+
+    int i = 0;
+    multiset<long long> us = {};
+    for (int j = 0; j <= i; j++)
+    {
+        us.insert(-1 * v[j]);
+        if (j == i && us.size() >= 3)
+        {
+            cout << (*us.begin()) * (*(++us.begin())) * (*(++(++us.begin()))) * -1 << endl;
+            // cout<<(*us.begin())<<" "<<(*(++us.begin()))<<" "<<(*(++(++us.begin())))<<endl;
+            // for(auto value : us){
+            //     cout<<value<<" ";
+            // }
+            // cout<<endl;
+        }
+        else if (j == i && us.size() < 3)
+        {
+            cout << defaultValue << "\n";
+        }
+        i++;
+        if(i == n){
+            break;
+        }
+    }
+
+    return 0;
+}
